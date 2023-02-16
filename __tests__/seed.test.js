c9ededd6924662ef4365a04cb09c480718fb4902
infoomics <sbx_94@hotmail.com>
2023-02-16 16:15:52 +0000

    complete insertTreasures() and its utility funcs

--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -1,53 +1,75 @@
-const { arrangeShopsData, prepareTreasuresData, } = require('../db/seed');
+const { arrangeShopsData, prepareTreasuresData, arrangeTreasuresData } = require('../db/seed');
 const { shopData, treasureData } = require('../db/data/test-data');
-
 
 describe('arrangeShopsData', () => {
     it('arrangeShopsData should return array of arrays containing correct information', () => {
-    expect(arrangeShopsData(shopData)).toEqual([
-        [  'shop-b',  'firstname-b',  'slogan-b' ],
-        [  'shop-d',  'firstname-c',  'slogan-d' ],
-        [  'shop-e',  'firstname-d',  'slogan-e' ],
-        [  'shop-f',  'firstname-e',  'slogan-f' ],
-        [  'shop-g',  'firstname-f',  'slogan-g' ],
-        [  'shop-h',  'firstname-a',  'slogan-h' ],
-        [  'shop-i',  'firstname-g',  'slogan-i' ],
-        [  'shop-a',  'firstname-h',  'slogan-a' ],
-        [  'shop-j',  'firstname-i',  'slogan-j' ],
-        [  'shop-k',  'firstname-j',  'slogan-k' ],
-        [  'shop-c',  'firstname-c',  'slogan-c' ]
-      ]);
-    })
-})
+        expect(arrangeShopsData(shopData)).toEqual([
+            [  'shop-b',  'firstname-b',  'slogan-b' ],
+            [  'shop-d',  'firstname-c',  'slogan-d' ],
+            [  'shop-e',  'firstname-d',  'slogan-e' ],
+            [  'shop-f',  'firstname-e',  'slogan-f' ],
+            [  'shop-g',  'firstname-f',  'slogan-g' ],
+            [  'shop-h',  'firstname-a',  'slogan-h' ],
+            [  'shop-i',  'firstname-g',  'slogan-i' ],
+            [  'shop-a',  'firstname-h',  'slogan-a' ],
+            [  'shop-j',  'firstname-i',  'slogan-j' ],
+            [  'shop-k',  'firstname-j',  'slogan-k' ],
+            [  'shop-c',  'firstname-c',  'slogan-c' ]
+        ]);
+    });
+});
+
 describe('prepareTreasuresData', () => {
-    const shopsWithId =   [
-    { shop_name: 'shop-b', owner: 'firstname-b', slogan: 'slogan-b', shop_id: 3 },
-    { shop_name: 'shop-d', owner: 'firstname-c', slogan: 'slogan-d', shop_id: 5 }
-];
+    const shopsWithId = [
+        { shop_name: 'shop-b', owner: 'firstname-b', slogan: 'slogan-b', shop_id: 3 },
+        { shop_name: 'shop-d', owner: 'firstname-c', slogan: 'slogan-d', shop_id: 5 }
+    ];
+
     it('prepareTreasuresData should return inserted shops and treasure data', () => {
-    const twoTreasures =   [
-        {
-        treasure_name: 'treasure-a',
-        colour: 'turquoise',
-        age: 200,
-        cost_at_auction: '20.00',
-        shop: 'shop-b'
-      },
-      {
-        treasure_name: 'treasure-d',
-        colour: 'azure',
-        age: 100,
-        cost_at_auction: '1001.00',
-        shop: 'shop-d'
-      }
-    ]
-        expect(prepareTreasuresData(shopsWithId, twoTreasures)).toEqual([
+        const twoTreasures = [
             {
+                treasure_name: 'treasure-a',
+                colour: 'turquoise',
+                age: 200,
+                cost_at_auction: '20.00',
+                shop: 'shop-b'
+            },
+            {
+                treasure_name: 'treasure-d',
+                colour: 'azure',
+                age: 100,
+                cost_at_auction: '1001.00',
+                shop: 'shop-d'
+            }
+        ];
+
+        expect(prepareTreasuresData(twoTreasures, shopsWithId)).toEqual([
+            {
+                treasure_name: 'treasure-a',
+                colour: 'turquoise',
+                age: 200,
+                cost_at_auction: '20.00',
+                shop_id: 3
+            },
+            {
+                treasure_name: 'treasure-d',
+                colour: 'azure',
+                age: 100,
+                cost_at_auction: '1001.00',
+                shop_id: 5
+            }
+        ]);
+    });
+});
+
+describe('arrangeTreasuresData', () => {
+    const treasuresWithShopId = [
+        {
             treasure_name: 'treasure-a',
             colour: 'turquoise',
             age: 200,
             cost_at_auction: '20.00',
-            shop_id: 3
+            shop_id: 4
           },
           {
             treasure_name: 'treasure-d',
@@ -56,6 +78,12 @@ describe('prepareTreasuresData', () => {
             cost_at_auction: '1001.00',
             shop_id: 5
           }
-        ])
-})
-})
+    ];
+
+    it('arrangeTreasuresData should return array of arrays containing correct information', () => {
+        expect(arrangeTreasuresData(treasuresWithShopId)).toEqual([
+            [  'treasure-a',  'turquoise',  200, '20.00',  4],
+            [  'treasure-d',  'azure',  100, '1001.00',  5]
+        ]);
+    });
+});
--- db/seed.js
+++ db/seed.js
@@ -19,8 +19,9 @@ const seed = ({ shopData, treasureData }) => {
 		return insertShops(shopData);
 	})
 	.then((insertedShops) => {
-		const lookUpShops = prepareTreasuresData(insertedShops, treasureData);
-		return insertTreasures(lookUpShops);
+		const treasuresWithShopId = prepareTreasuresData(treasureData, insertedShops);
+
+		return insertTreasures(treasuresWithShopId);
 	})
 };
 
@@ -39,13 +40,17 @@ function createTreasures() {
 	return db.query(
 		`CREATE TABLE treasures (
 			treasure_id SERIAL PRIMARY KEY,
-			treasure_name VARCHAR(40) NOT NULL,
+			treasure_name VARCHAR(100) NOT NULL,
 			colour VARCHAR(40) NOT NULL,
 			age INT NOT NULL,
 			cost_at_auction FLOAT(8) NOT NULL,
 			shop_id INT REFERENCES shops(shop_id)
 		);`
 	);
+}
+
+function arrangeShopsData(shopData) {
+	return shopData.map(shop => [shop.shop_name, shop.owner, shop.slogan]);
 }
 
 function insertShops(shopData) {
@@ -60,22 +65,43 @@ function insertShops(shopData) {
 	.then(result => result.rows);
 }
 
-function arrangeShopsData(shopData) {
-	return shopData.map(shop => [shop.shop_name, shop.owner, shop.slogan]);
+function prepareTreasuresData(treasureData, insertedShops) {
+	if (treasureData.length === 0) return [];
+
+	treasureData = JSON.parse( JSON.stringify(treasureData) );
+	insertedShops = JSON.parse( JSON.stringify(insertedShops) );
+
+	const shopsLookup = {};
+	insertedShops.forEach(shop => shopsLookup[shop.shop_name] = shop.shop_id);
+
+	return treasureData.map(treasure => {
+		treasure.shop_id = shopsLookup[treasure.shop];
+		
+		delete treasure.shop;
+		
+		return treasure;
+	});
 }
 
-function prepareTreasuresData(insertedShops, treasureData) {
-
-
+function arrangeTreasuresData(treasures) {
+	return treasures.map(treasure => [treasure.treasure_name, treasure.colour, treasure.age, treasure.cost_at_auction, treasure.shop_id]);
 }
 
-function insertTreasures() {
+function insertTreasures(treasuresWithShopId) {
+	const insertTreasuresStr = format(
+		`INSERT INTO treasures (treasure_name, colour, age, cost_at_auction, shop_id)
+		VALUES %L
+		RETURNING *;`,
+		arrangeTreasuresData(treasuresWithShopId)
+	);
 	
-	
+	return db.query(insertTreasuresStr)
+	.then(result => result.rows);
 }
 
 module.exports = {
 	seed,
 	arrangeShopsData,
 	prepareTreasuresData,
+	arrangeTreasuresData
 };
